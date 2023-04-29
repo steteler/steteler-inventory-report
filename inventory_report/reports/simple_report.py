bfa9b7c28660eb604f3bad46783689029a04878c
kikuti <joaovictorkikuti@gmail.com>
2023-04-29 16:02:14 -0300

    feat: terminado requisito 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,23 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products):
+        fabricacao_mais_antiga = min(
+            product["data_de_fabricacao"] for product in products
+        )
+        validade_mais_proxima = min(
+            product["data_de_validade"]
+            for product in products
+            if datetime.strptime(product["data_de_validade"], "%Y-%m-%d")
+            > datetime.now()
+        )
+        empresas = list(product["nome_da_empresa"] for product in products)
+        empresa_mais_produtos = max(empresas, key=lambda x: empresas.count(x))
+
+        return (
+            f"Data de fabricação mais antiga: {fabricacao_mais_antiga}\n"
+            f"Data de validade mais próxima: {validade_mais_proxima}\n"
+            f"Empresa com mais produtos: {empresa_mais_produtos}"
+        )
