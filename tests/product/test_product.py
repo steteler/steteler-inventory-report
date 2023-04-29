d2d635f7559bd55c3d7825406dcef87e854048a7
kikuti <joaovictorkikuti@gmail.com>
2023-04-29 01:25:02 -0300

    feat: terminado primeiro requisito

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        9,
+        "morango",
+        "Emma Corporation",
+        "2010-10-21",
+        "2011-10-21",
+        369,
+        "jogue fora",
+    )
+
+    assert product.__repr__() == (
+        "O produto morango fabricado "
+        "em 2010-10-21 por Emma Corporation com validade "
+        "até 2011-10-21 precisa ser armazenado jogue fora."
+    )
