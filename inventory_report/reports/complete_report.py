46e8107fc73f2d8c9eeece06636bcf223f7ea276
kikuti <joaovictorkikuti@gmail.com>
2023-04-30 19:37:20 -0300

    feat: terminado requisito 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,18 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products):
+        report = SimpleReport.generate(products)
+        empresa_quantidade = {
+            product["nome_da_empresa"]: 0 for product in products
+        }
+        for product in products:
+            empresa_quantidade[product["nome_da_empresa"]] += 1
+
+        report += "\nProdutos estocados por empresa:\n"
+        for empresa, quantidade in empresa_quantidade.items():
+            report += f"- {empresa}: {quantidade}\n"
+
+        return report
