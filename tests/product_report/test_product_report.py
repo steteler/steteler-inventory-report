f7900d9ab92b53954b448b1d22f21fb51f1ec70d
kikuti <joaovictorkikuti@gmail.com>
2023-05-01 23:49:21 -0300

    refactor: código refatorado com o black

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -11,8 +11,10 @@ def test_relatorio_produto():
         1514,
         "no congelador",
     )
-    expect = "O produto Emma Cabelos fabricado" \
-        " em 1978 por Emma Corporation com validade até 1979" \
+    expect = (
+        "O produto Emma Cabelos fabricado"
+        " em 1978 por Emma Corporation com validade até 1979"
         " precisa ser armazenado no congelador."
+    )
 
     assert expect == product1.__repr__()
