373d5c6a91e68320eb6cb7737171bf5bd414ade6
kikuti <joaovictorkikuti@gmail.com>
2023-05-01 23:04:46 -0300

    feat: terminado requisito 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product1 = Product(
+        1,
+        "Emma Cabelos",
+        "Emma Corporation",
+        "1978",
+        "1979",
+        1514,
+        "no congelador",
+    )
+    expect = "O produto Emma Cabelos fabricado" \
+        " em 1978 por Emma Corporation com validade até 1979" \
+        " precisa ser armazenado no congelador."
+
+    assert expect == product1.__repr__()
